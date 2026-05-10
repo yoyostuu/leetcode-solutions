f0233a33003bce2f1a22dac72a0ce39dab9daad1
yoyostuu <12ananyamoharana34@gmail.com>
2026-05-10 23:58:54 +0530

    Add files via upload

---
+++ prob4.java
@@ -0,0 +1,28 @@
+class prob4 {
+    public String intToRoman(int num) {
+        
+        int[] values = {
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4, 1
+        };
+
+        String[] romans = {
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV", "I"
+        };
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < values.length; i++) {
+            
+            while (num >= values[i]) {
+                result.append(romans[i]);
+                num -= values[i];
+            }
+        }
+
+        return result.toString();
+    }
+}
