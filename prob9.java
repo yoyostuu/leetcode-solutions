01bcf9abb42ae72f000bfedd0ea12863b315d066
yoyostuu <12ananyamoharana34@gmail.com>
2026-05-26 23:31:56 +0530

    Add files via upload

---
+++ prob9.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public boolean isValid(String s) {
+
+        Stack<Character> st = new Stack<>();
+
+        for(char ch : s.toCharArray()) 
+        {
+            if(ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            }        
+            else {
+                if(st.isEmpty()) {
+                    return false;
+                }
+
+                char top = st.pop();
+
+                if(ch == ')' && top != '(') {
+                    return false;
+                }
+
+                if(ch == '}' && top != '{') {
+                    return false;
+                }
+
+                if(ch == ']' && top != '[') {
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
